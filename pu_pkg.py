95b7720095d4c9d87e9dec7ee7e901229c1e69a6
caravaned <47841017+caravaned@users.noreply.github.com>
2021-05-20 16:44:29 +0800

    Add files via upload
    发现PU口袋活动首页并非最新活动
    所以不再使用if判断新增 重写txt文档
    改用is_new方法 append进txt

--- pu_pkg.py
+++ pu_pkg.py
@@ -4,6 +4,11 @@ import requests
 from lxml import etree
 cookie = "这里放你的cookie"
 prt_url = "这里放你的钉钉机器人API"
+# 参考文档：https://developers.dingtalk.com/document/app/custom-robot-access
+my_collage = "这里放你的学院名称"
+# eg: 计算机科学与信息工程学院
+
+
 header = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
     "cookie": cookie,
@@ -12,7 +17,6 @@ header = {
 
 def main():
     after = []
-    is_new = False
     try:
         with open("a.txt", "r", newline='') as f2:
             l = f2.readlines()
@@ -20,26 +24,25 @@ def main():
                 new = i.strip()
                 after.append(new)
     except Exception:
-        print("捕获到文件异常 执行覆盖写入操作")
-        is_new = True
+        print("捕获到文件异常")
+
+
     titles = download1("https://ayit.pocketuni.net/index.php?app=event&mod=School&act=board")
-    #titles = download1("http://127.0.0.1")
     for title in titles:
         if title.text in after:
             pass
         else:
             new_url = title.xpath("./@href")[0]
             rpt_title = title.text
-            download2(new_url,rpt_title)
-            is_new = True
+            download2(new_url, rpt_title)
+            is_new(rpt_title)
 
-    if is_new:
-        with open("a.txt", "w") as f3:
-            print("数据已更新")
-            for title in titles:
-                f3.write(title.text)
-                f3.write("\n")
-                print(title.text)
+def is_new(new_title):
+    with open("a.txt", "a") as f3:
+        print("数据已更新")
+        f3.write(new_title)
+        f3.write("\n")
+
 
 def download1(url):
     res = requests.get(url=url, headers=header)
@@ -56,19 +59,18 @@ def download2(url,rpt_title):
     keys = root.xpath("//span[contains(@class,'b1')]")
     values = root.xpath("//span[contains(@class,'b1')]/following::text()[1]")
     data = {}
-    data["活动院系："] = "无院系"
+    data["活动院系："] = "无院系要求"
     for i in range(len(keys)):
         value = values[i].strip()
         value = " ".join(value.split())
         data[keys[i].text.strip()] = value
     report(rpt_title,data)
-    print(data)
+    # print(data)
 
 def report(rpt_title,data):
     College = data["活动院系："]
-
     # 计算机科学与信息工程学院
-    if "全部" == College or "计算机科学与信息工程学院" == College or "无院系" == College:
+    if "全部" == College or my_collage == College or "无院系要求" == College:
         json1 = {
         "at": {
             "isAtAll": "True"
@@ -89,18 +91,22 @@ def report(rpt_title,data):
             },
 
         }
+        # print(json1)
+        # print(json2)
         requests.post(url=prt_url, headers=header, json=json1)
         requests.post(url=prt_url, headers=header, json=json2)
 
     else:
         json = {
             "text": {
-                "content": "收到一条"+College+"新活动"
+                "content": "收到一条 "+College+" 新活动"
             },
             "msgtype": "text"
         }
+        # print(json)
         requests.post(url=prt_url, headers=header, json=json)
 while True:
-    time.sleep(30)
-    print("check")
+    localtime = time.asctime(time.localtime(time.time()))
+    time.sleep(100)
+    # print("check "+localtime)
     main()
